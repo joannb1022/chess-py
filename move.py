364812bd57bda32af90e05a89591f86b9479bc2b
APrzywieczerski <54894484+Monatyr@users.noreply.github.com>
2021-05-09 14:41:59 +0200

    move

--- move.py
+++ move.py
@@ -1,7 +1,10 @@
 class Move:
     
-    def __init__(self, piece, s_pos, t_pos, taken_piece):
+    def __init__(self, piece, s_pos, t_pos, taken_piece, en_passant = False, castle = None, promotion = False):
         self.piece = piece
         self.start_pos = s_pos
         self.end_pos = t_pos
         self.taken_piece = taken_piece
+        self.en_passant = en_passant
+        self.castle = castle
+        self.promotion = promotion
