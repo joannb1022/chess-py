4e7e6a33da5f584be4f07e790ca869764a301d10
APrzywieczerski <54894484+Monatyr@users.noreply.github.com>
2021-05-21 18:52:57 +0200

    move to_string

--- move.py
+++ move.py
@@ -1,6 +1,6 @@
 class Move:
     
-    def __init__(self, piece, s_pos, t_pos, taken_piece, en_passant = False, castle = None, promotion = False):
+    def __init__(self, piece, s_pos, t_pos, taken_piece = None, en_passant = False, castle = None, promotion = False):
         self.piece = piece
         self.start_pos = s_pos
         self.end_pos = t_pos
@@ -8,3 +8,7 @@ class Move:
         self.en_passant = en_passant
         self.castle = castle
         self.promotion = promotion
+
+    def to_string(self):
+        takes = 'x' if self.taken_piece is not None else ''
+        return f'{self.piece.__class__.__name__[0]}{self.start_pos[1]+1}{chr(self.start_pos[0]+95)}{takes}{self.end_pos[1]+1}{chr(self.end_pos[0]+95)}'
